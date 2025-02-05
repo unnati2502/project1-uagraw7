f227ffb6bbb480364cfcd8cb9351693df39e2010
unnati2502 <unnati_02@Unnati.>
2025-02-05 17:02:21 -0500

    Basic app functionality done

--- app.py
+++ app.py
@@ -1,1 +1,24 @@
+import flask
+from flask import render_template
+import random
+from movie import tmdb_data, wiki_data, Movie_id_list
+app= flask.Flask(__name__)
+
+@app.route("/")
+def main():
+    movie_id= random.choice(Movie_id_list)
+    movie_data= tmdb_data(movie_id)
+    if movie_data:
+        title= movie_data["original_title"]
+        tagline=movie_data["tagline"]
+        genre= movie_data["genres"]["name"]
+        poster_path= movie_data["poster_path"]
+        poster_url=f"https://image.tmdb.org/t/p/w500{poster_path}" if poster_path else ""
+        wiki_url= wiki_data(title)
+        
+        return render_template('index.html', title= title, tagline= tagline,genre=genre, poster_url= poster_url, wiki_url= wiki_url )
+    else :
+        return render_template('error.html', message="Failed to load movie data")
+
+app.run
 
---
+++ movie.py
@@ -0,0 +1,41 @@
+import requests
+import os
+from dotenv import find_dotenv, load_dotenv
+
+
+load_dotenv(find_dotenv())
+Tmdb_key = os.getenv("TMDB-API")
+Movie_id_list= [157336, 550, 680]
+
+#Base url
+Base_url="https://api.themoviedb.org/3/movie"
+WIKI_API_URL = "https://en.wikipedia.org/w/api.php"
+#https://api.themoviedb.org/3/movie/550?api_key=
+#function to load data from tmdb
+def tmdb_data(movie_id):
+    response = requests.get(Base_url, params={"q": movie_id, "api-key": Tmdb_key})
+    if response.status_code==200:
+        return response.json()
+    return None
+
+#function to load the wikipedia link
+def wiki_data(movie_title):
+    params={
+        "action": "query",
+        "format": "json",
+        "title": movie_title,
+        "prop": "info",
+        "inprop": "url"
+    }
+    
+    response= requests .get(WIKI_API_URL, params= params)
+    data= response.json()#contains the link
+    pages= data.get("query", {}).get("pages", {})
+    for page_id, page_data in pages.items():
+        if "fullurl" in page_data:
+            return page_data["fullurl"]
+    return "https://en.wikipedia.org/wiki/Main_Page"
+
+
+
+
